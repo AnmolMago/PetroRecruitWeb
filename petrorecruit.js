09f02d81ce9869db83a9a26e1c3ece9b8cffd8ac
Anmol Mago <anmolmago@gmail.com>
2016-05-20 03:23:13 -0400

    full, basic, unresponsive

--- petrorecruit.js
+++ petrorecruit.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+	
 	$("#menu").click(function(){
-		$("")
+		if($(window).width() < 875){
+			$("#navbtns").toggle()
+		}
 	});
+	
+	
+	$(window).resize(function() {onResize();});
+	setTimeout( function() { onResize(); },25);
 });
+
+function onResize() {
+	if($(window).width() >= 875){
+			$("#navbtns").css("display", "");
+	}
+}
